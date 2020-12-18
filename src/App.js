b1c4d5aab065017989bf5a676006674e95790a1c
Swathi Savalge <ssavalge@walmartlabs.com>
2020-12-18 15:09:44 -0800

    New repo for learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import './names.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+    state = {fName: '', lName: ''};
+
+    provideName = () => {
+      fetch('test.walmart.com/person', { method : 'post', params: {fname: this.state.fName, lname: this.state.lName}})
+      .then(function(fname,lname) {
+        console.log(fname, lname);
+      });
+    }
+
+    changeName = () => {
+      this.setState({lName: 'foo', fName: 'bar'});
+    }
+
+    emptyNames = () => {
+      this.setState({lName: '', fName: ''});
+    }
+    
+    render() {
+        return <div className="ui container" style={{marginTop: '10px'}}>
+        <label>FirstName</label>
+        <input type='text' value={this.state.fName} onChange={(e) => this.setState({fName: e.target.value})}/>
+        <label>LastName</label>
+        <input type='text' value={this.state.lName} onChange={(e) => this.setState({lName: e.target.value})}/>
+        <button onClick={this.provideName} >Submit</button>
+        <button onClick={this.changeName}> Change Name</button>
+        <button onClick={this.emptyNames}> Empty Names</button>
     </div>
-  );
+    }
 }
 
 export default App;
